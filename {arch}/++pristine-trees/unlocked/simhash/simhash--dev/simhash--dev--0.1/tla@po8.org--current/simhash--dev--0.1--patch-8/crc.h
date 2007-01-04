b3186e0be13c3a1632d2c059336e5801d2c8863a
Bart Massey <bart@cs.pdx.edu>
2007-01-03 19:47:17 -0800

    removed old arch dir

--- {arch}/++pristine-trees/unlocked/simhash/simhash--dev/simhash--dev--0.1/tla@po8.org--current/simhash--dev--0.1--patch-8/crc.h
+++
@@ -1,1 +0,0 @@
-extern int hash_crc32(char *buf, int i0, int nbuf);
--- {arch}/++pristine-trees/unlocked/simhash/simhash--dev/simhash--dev--0.1/tla@po8.org--current/simhash--dev--0.1--patch-8/hash.h
+++
@@ -1,4 +0,0 @@
-extern void hash_reset(int);
-extern int hash_contains(unsigned);
-extern void hash_insert(unsigned);
-extern int hash_delete(unsigned);
